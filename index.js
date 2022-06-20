e23ec570c995a251d1285dfba4a6c493a341a71b
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-06-20 18:45:27 +0530

    main

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const Skincaredata = require("./db.json");
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send(`skincare data running `);
+});
+
+app.get("/skindata", (req, res) => {
+  res.send(Skincaredata);
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
